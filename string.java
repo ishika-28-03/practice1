f4aeae6f448d6ea03e14245234a29ab8ac49f40d
ishika prasad <dp878917@gmail.com>
2023-08-17 10:16:05 +0530

    commit

--- string.java
+++ string.java
@@ -1,9 +1,13 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.io.*;
 import java.util.Scanner;
 import java.lang.*;
+import java.util.Arrays;
 public class string {
     public static void main(String[] arg){
-        String name="Ishika";
+        /*String name="Ishika";
          name = name.concat(" Prasad"); 
         System.out.print(name);
         String a;
@@ -12,8 +16,15 @@ public class string {
         System.out.print(a);
         String demoString = new String("Bhubaneswar");
         System.out.print(demoString);
+        */
+        //MIN AND MAX
+        int arr[] ={1000,11,445,87,330,981,1};
+        int arr_size=arr.length;
+        Pair minmax = getMinMax(arr,arr_size);
 
+        
 
 
     }
 }
+
